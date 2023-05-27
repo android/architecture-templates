fca6e5ae87b0700183a4666d3366500f49f8da72
Moyinoluwa <moyinoluwaa@gmail.com>
2023-05-27 07:41:21 +0100

    Replace produceState with collectAsStateWithLifecycle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -102,6 +102,7 @@ dependencies {
     kaptTest(libs.hilt.android.compiler)
 
     // Arch Components
+    implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.hilt.navigation.compose)
