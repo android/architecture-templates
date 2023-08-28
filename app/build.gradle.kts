4ea60ee109b524f7c17aa259678ab7166c21c122
Jose Alcérreca <JoseAlcerreca@users.noreply.github.com>
2023-08-28 14:35:20 +0200

    Sets compile and target SDKs to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,12 +25,12 @@ plugins {
 
 android {
     namespace = "android.template"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "android.template"
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
