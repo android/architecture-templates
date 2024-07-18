9f2d1edaebf55bb831de82a41e8536e57166dc08
jsytal1 <jsytal1@gmail.com>
2024-07-18 04:29:54 -0400

    Update Kotlin and KSP versions and migrate to Compose gradle plugin (#159)
    * Bump Kotlin and KSP versions to 2.0.0
    
    * Remove the androidxComposeCompiler version and its usage in composeOptions.
    
    * Add Compose compiler plugin in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,6 +21,7 @@ plugins {
     alias(libs.plugins.kotlin.kapt)
     alias(libs.plugins.hilt.gradle)
     alias(libs.plugins.ksp)
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
@@ -67,10 +68,6 @@ android {
         buildConfig = false
         renderScript = false
         shaders = false
-    }
-
-    composeOptions {
-        kotlinCompilerExtensionVersion = libs.versions.androidxComposeCompiler.get()
     }
 
     packagingOptions {
