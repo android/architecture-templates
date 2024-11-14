b4add8a5abad07fa591159ba9173a94dd38ce2be
Jose Alcérreca <JoseAlcerreca@users.noreply.github.com>
2024-11-14 15:32:03 +0100

    Compile SDK = 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,12 +26,12 @@ plugins {
 
 android {
     namespace = "android.template"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "android.template"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
