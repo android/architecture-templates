6bbc5c9eed77bd25cc8327ef8b101fee89c66ad4
Jose Alcérreca <JoseAlcerreca@users.noreply.github.com>
2025-09-01 11:07:48 +0200

    Update minSdk version from 21 to 23

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
 
     defaultConfig {
         applicationId = "android.template"
-        minSdk = 21
+        minSdk = 23
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
