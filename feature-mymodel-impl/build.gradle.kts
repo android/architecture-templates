080c416d47ebfaf5b02d1c9d2b1751bb71373f6c
Don Turner <donturner@google.com>
2026-02-12 19:29:26 +0000

    Minor fixes

--- feature-mymodel-impl/build.gradle.kts
+++ feature-mymodel-impl/build.gradle.kts
@@ -65,7 +65,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
-    implementation(libs.androidx.hilt.navigation.compose)
+    implementation(libs.androidx.hilt.lifecycle.viewmodel.compose)
 
     // Compose
     implementation(libs.androidx.compose.ui)
--- feature-mymodel-impl/src/main/java/android/template/feature/mymodel/ui/MyModelScreen.kt
+++ feature-mymodel-impl/src/main/java/android/template/feature/mymodel/ui/MyModelScreen.kt
@@ -35,7 +35,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation3.runtime.NavKey
 
