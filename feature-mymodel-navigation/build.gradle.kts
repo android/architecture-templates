966c97ed4e0898e15b38a51eaa11149abd803038
Don Turner <donturner@google.com>
2026-02-18 17:19:24 +0000

    Change namespace for navigation module

--- feature-mymodel-navigation/build.gradle.kts
+++ feature-mymodel-navigation/build.gradle.kts
@@ -23,7 +23,7 @@ plugins {
 }
 
 android {
-    namespace = "android.template.feature.mymodel"
+    namespace = "android.template.feature.mymodel.navigation"
     compileSdk = 36
     defaultConfig {
         minSdk = 23
